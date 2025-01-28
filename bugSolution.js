06e148093734cd6866b8df674fa3559f6676ad47
BugHunter-X <askzenai@gmail.com>
2025-01-28 11:54:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Image } from 'expo-image';
+
+const MyImage = ({ uri }) => {
+  const encodedUri = encodeURIComponent(uri);
+  return (
+    <Image
+      source={{ uri: encodedUri }}
+      style={{ width: 200, height: 200 }}
+    />
+  );
+};
+
+export default MyImage;
